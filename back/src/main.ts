f119cdc2e7f23e34a1f647ccc6d34fb018a651e0
unknown <mihail.diweew@yandex.ru>
2024-06-01 14:06:06 +0300

    delete public assets

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,18 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma.service';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create(AppModule);
 
   const prismaService = app.get(PrismaService);
   // await prismaService.enableShutdownHooks(app)
-
-  app.useStaticAssets(join(__dirname, '..', 'src', 'uploads'), {
-    prefix: '/uploads/',
-  });
 
   app.setGlobalPrefix('api')
   app.enableCors()
